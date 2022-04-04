fd208f149119f871618ff93fa848f3309120f8d1
laserchemist <jonathan.m.smith@temple.edu>
2022-04-04 16:42:40 -0400

    Machine Learning Laboratory
    New laboratory using k nearest neighbors

---
+++ spring2022/week12/tests/q7.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> example_nn_row.num_rows == 1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
